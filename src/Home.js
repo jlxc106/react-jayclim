22a44ede47efc73adf4aa2865e0101fbb814374c
jlxc106 <jaylim106@gmail.com>
2020-03-22 20:41:54 -0700

    minor clean up + updated social media meta tags

--- src/Home.js
+++ src/Home.js
@@ -49,12 +49,9 @@ export default class Home extends React.Component {
   }
 
   render() {
-
     return (
       <>
-        <ParticlesComponent
-          ref={this.canvasRef}
-        />
+        <ParticlesComponent ref={this.canvasRef} />
         <div className="main-text-container">
           <h1>Jay Lim</h1>
           <div>
@@ -107,11 +104,15 @@ export default class Home extends React.Component {
           className={`${this.state.isTop ? "" : ""} profileSection`}
         >
           <Grid className="img-container" item xs={12} sm={3}>
-            <img className="v-center profile-img" src="jay.jpeg" alt="jay"></img>
+            <img
+              className="v-center profile-img"
+              src="jay.jpeg"
+              alt="jay"
+            ></img>
           </Grid>
 
           <Grid item xs={12} sm={9}>
-            <p className="section-font" style={{marginLeft: "10px"}}>
+            <p className="section-font" style={{ marginLeft: "10px" }}>
               Hello, my name is <b>Jay Lim</b>; I'm a{" "}
               <b>web/iOS software engineer</b>, with a passion for learning and
               solving problems.
@@ -136,87 +137,187 @@ export default class Home extends React.Component {
           </Grid>
           <Grid className="section-desc" item xs={12} sm={9}>
             <div className="">
-              <h4 className="section-font-sec exp-title">
-                HaloSentient 
-              </h4>
+              <h4 className="section-font-sec exp-title">HaloSentient</h4>
               <h5 className="exp-sub-title">FullStack Developer</h5>
-              <p className="exp-desc">Responsible for creating a minimum viable product for 
-              a financial technology (FinTech) application. Audited an interview based 
-              web(Angular/Node.js) & mobile(iOS & Android) application by testing client 
-              & reading source code. Developed & helped design multiple websites using 
-              Wordpress including company website. Created futuristic web app using 
-              React & Redux for a security-based project that provided latest data to the 
-              UI. Created a backend API layer using Node.JS and MongoDB that interfaces 
-              with security sensors. Utilized Socket.IO to send real-time data updates to 
-              the client.</p>
+              <p className="exp-desc">
+                Responsible for creating a minimum viable product for a
+                financial technology (FinTech) application. Audited an interview
+                based web(Angular/Node.js) & mobile(iOS & Android) application
+                by testing client & reading source code. Developed & helped
+                design multiple websites using Wordpress including company
+                website. Created futuristic web app using React & Redux for a
+                security-based project that provided latest data to the UI.
+                Created a backend API layer using Node.JS and MongoDB that
+                interfaces with security sensors. Utilized Socket.IO to send
+                real-time data updates to the client.
+              </p>
               <ul className="exp-skills">
-                <li><span>Web</span></li>
-                <li><span>Javascript</span></li>
-                <li><span>Wordpress</span></li>
-                <li><span>React</span></li>
-                <li><span>Redux</span></li>
-                <li><span>Node.js</span></li>
-                <li><span>SQL</span></li>
-                <li><span>NoSQL</span></li>
-                <li><span>Express</span></li>
-                <li><span>MongoDB</span></li>
-                <li><span>GCP</span></li>
-                <li><span>Heroku</span></li>
-                <li><span>Socket.io</span></li>
-                <li><span>HTML</span></li>
-                <li><span>CSS</span></li>
-                <li><span>Git</span></li>
-                <li><span>Apache</span></li>
+                <li>
+                  <span>Web</span>
+                </li>
+                <li>
+                  <span>Javascript</span>
+                </li>
+                <li>
+                  <span>Wordpress</span>
+                </li>
+                <li>
+                  <span>React</span>
+                </li>
+                <li>
+                  <span>Redux</span>
+                </li>
+                <li>
+                  <span>Node.js</span>
+                </li>
+                <li>
+                  <span>SQL</span>
+                </li>
+                <li>
+                  <span>NoSQL</span>
+                </li>
+                <li>
+                  <span>Express</span>
+                </li>
+                <li>
+                  <span>MongoDB</span>
+                </li>
+                <li>
+                  <span>GCP</span>
+                </li>
+                <li>
+                  <span>Heroku</span>
+                </li>
+                <li>
+                  <span>Socket.io</span>
+                </li>
+                <li>
+                  <span>HTML</span>
+                </li>
+                <li>
+                  <span>CSS</span>
+                </li>
+                <li>
+                  <span>Git</span>
+                </li>
+                <li>
+                  <span>Apache</span>
+                </li>
               </ul>
             </div>
             <div className="">
-              <h4 className="section-font-sec exp-title">
-                SpotLite 
-              </h4>
+              <h4 className="section-font-sec exp-title">SpotLite</h4>
               <h5 className="exp-sub-title">Software Engineer</h5>
-              <p className="exp-desc">Developed backend API using Golang on the GCP platform(Firebase,
-              Storage, Spanner, Functions) to provide backend services for an iOS application. Developed an iOS app using Swift(SwiftUI framework with UIKit components) – building UIs, authentication system w/ biometric auth, video cropping & thumbnail generation, state management, CRUD operations
-              with backend & Core Data, sample data population scripts, background app refresh, etc. Responsible for synchronizing data on backend with data on the client. Continuously improved the quality of the codebase with quicker and more efficient solutions. Built the main and profile preview website from scratch using React and an API route to populate data.</p>
+              <p className="exp-desc">
+                Developed backend API using Golang on the GCP platform(Firebase,
+                Storage, Spanner, Functions) to provide backend services for an
+                iOS application. Developed an iOS app using Swift(SwiftUI
+                framework with UIKit components) – building UIs, authentication
+                system w/ biometric auth, video cropping & thumbnail generation,
+                state management, CRUD operations with backend & Core Data,
+                sample data population scripts, background app refresh, etc.
+                Responsible for synchronizing data on backend with data on the
+                client. Continuously improved the quality of the codebase with
+                quicker and more efficient solutions. Built the main and profile
+                preview website from scratch using React and an API route to
+                populate data.
+              </p>
               <ul className="exp-skills">
-                <li><span>iOS</span></li>
-                <li><span>Web</span></li>
-                <li><span>Golang</span></li>
-                <li><span>Swift</span></li>
-                <li><span>SwiftUI</span></li>
-                <li><span>UIKit</span></li>
-                <li><span>GCP</span></li>
-                <li><span>Firebase</span></li>
-                <li><span>SQL</span></li>
-                <li><span>Alamofire</span></li>
-                <li><span>API</span></li>
-                <li><span>Core Data</span></li>
-                <li><span>Biometric Auth</span></li>
-                <li><span>React</span></li>
-                <li><span>Javascript</span></li>
-                <li><span>HTML</span></li>
-                <li><span>CSS</span></li>
-                <li><span>Git</span></li>
-                <li><span>Apache</span></li>
+                <li>
+                  <span>iOS</span>
+                </li>
+                <li>
+                  <span>Web</span>
+                </li>
+                <li>
+                  <span>Golang</span>
+                </li>
+                <li>
+                  <span>Swift</span>
+                </li>
+                <li>
+                  <span>SwiftUI</span>
+                </li>
+                <li>
+                  <span>UIKit</span>
+                </li>
+                <li>
+                  <span>GCP</span>
+                </li>
+                <li>
+                  <span>Firebase</span>
+                </li>
+                <li>
+                  <span>SQL</span>
+                </li>
+                <li>
+                  <span>Alamofire</span>
+                </li>
+                <li>
+                  <span>API</span>
+                </li>
+                <li>
+                  <span>Core Data</span>
+                </li>
+                <li>
+                  <span>Biometric Auth</span>
+                </li>
+                <li>
+                  <span>React</span>
+                </li>
+                <li>
+                  <span>Javascript</span>
+                </li>
+                <li>
+                  <span>HTML</span>
+                </li>
+                <li>
+                  <span>CSS</span>
+                </li>
+                <li>
+                  <span>Git</span>
+                </li>
+                <li>
+                  <span>Apache</span>
+                </li>
               </ul>
             </div>
             <div className="">
-            <h4 className="section-font-sec exp-title">
-                MaintStar 
-              </h4>
+              <h4 className="section-font-sec exp-title">MaintStar</h4>
               <h5 className="exp-sub-title">React Intern</h5>
-              <p className="exp-desc">Responsible for creating Query module using React front end. 
-              Utilized Semantic UI and Kendo UI for React to style HTML. Utilized Mobx framework 
-              with React to manage state. Pulled data from API route connected to MYSQL Server database. 
-              Component lets end users run reports using custom parameters and view pdf on the browser.</p>
+              <p className="exp-desc">
+                Responsible for creating Query module using React front end.
+                Utilized Semantic UI and Kendo UI for React to style HTML.
+                Utilized Mobx framework with React to manage state. Pulled data
+                from API route connected to MYSQL Server database. Component
+                lets end users run reports using custom parameters and view pdf
+                on the browser.
+              </p>
               <ul className="exp-skills">
-                <li><span>Web</span></li>
-                <li><span>React</span></li>
-                <li><span>Mobx</span></li>
-                <li><span>Semantic UI</span></li>
-                <li><span>Kendo UI</span></li>
-                <li><span>Javascript</span></li>
-                <li><span>HTML</span></li>
-                <li><span>CSS</span></li>
+                <li>
+                  <span>Web</span>
+                </li>
+                <li>
+                  <span>React</span>
+                </li>
+                <li>
+                  <span>Mobx</span>
+                </li>
+                <li>
+                  <span>Semantic UI</span>
+                </li>
+                <li>
+                  <span>Kendo UI</span>
+                </li>
+                <li>
+                  <span>Javascript</span>
+                </li>
+                <li>
+                  <span>HTML</span>
+                </li>
+                <li>
+                  <span>CSS</span>
+                </li>
               </ul>
             </div>
             <div></div>
@@ -232,7 +333,9 @@ export default class Home extends React.Component {
           container
           ref={this.skillRef}
           justify="center"
-          className={`${this.state.isTop ? "" : ""} skillSection section-font-sec`}
+          className={`${
+            this.state.isTop ? "" : ""
+          } skillSection section-font-sec`}
         >
           <Grid className="section-title" item xs={12} sm={3}>
             <h3 className="section-font">Skills</h3>
@@ -251,7 +354,6 @@ export default class Home extends React.Component {
                   <li>NoSQL</li>
                   <li>PHP</li>
                   <li>Matlab</li>
-                  
                 </ul>
               </Grid>
               <Grid item xs={6} sm={3}>
@@ -302,30 +404,34 @@ export default class Home extends React.Component {
             <div></div> */}
           </Grid>
           <Grid className="section-footer" item xs={12}>
-            <Grid container justify="center" alignItems="flex-end" style={{height:"100%"}}>
+            <Grid
+              container
+              justify="center"
+              alignItems="flex-end"
+              style={{ height: "100%" }}
+            >
               <Grid item xs={3}>
-              <b>Jay Lim</b>
+                <b>Jay Lim</b>
               </Grid>
               <Grid item xs={9}>
-              <a
-                href="/JAY_LIM_RESUME_MAR_20.pdf"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                <span>Resume</span>
-                {/* <i className="fab fa-github-alt"></i> Github */}
-              </a>
-              {/* <span>Resume</span> */}
-              {" "}
-              <a
-                href="https://www.github.com/jlxc106"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                <span>Github</span>
-                {/* <i className="fab fa-github-alt"></i> Github */}
-              </a>
-              {/* <span>Github</span> */}
+                <a
+                  href="/JAY_LIM_RESUME_MAR_20.pdf"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <span>Resume</span>
+                  {/* <i className="fab fa-github-alt"></i> Github */}
+                </a>
+                {/* <span>Resume</span> */}{" "}
+                <a
+                  href="https://www.github.com/jlxc106"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <span>Github</span>
+                  {/* <i className="fab fa-github-alt"></i> Github */}
+                </a>
+                {/* <span>Github</span> */}
               </Grid>
             </Grid>
           </Grid>
@@ -361,6 +467,5 @@ export default class Home extends React.Component {
         </Grid>
       </>
     );
-
   }
 }
