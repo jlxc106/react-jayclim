be717c81ed4ba40509dd332202c5dc34ba3dc663
Jay Lim <jaylim106@gmail.com>
2020-10-03 15:01:45 -0700

    u

--- src/App.test.js
+++ src/App.test.js
@@ -1,71 +1,29 @@
-const puppeteer = require('puppeteer');
-let browser
-const baseUrl = `http://127.0.0.1:3030/`
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
 
-beforeAll(async () => {
-  browser = await puppeteer.launch({
-    headless: true
+import App from "./App";
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+  HTMLCanvasElement.prototype.getContext = () => { 
+    // return whatever getContext has to return
+  };
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it("renders correctly", () => {
+  act(() => {
+    render(<App />, container);
   });
-})
-
-afterAll(() => {
-  browser.close();
-})
-
-test("1 is 1", () => {
-  expect(1).toBe(1)
-})
-// describe('Headless Browser Tests', () => {
-//   test('Particle JS Canvas Background Loads Properly', async () => {
-//     let page = await browser.newPage();
-//     await page.goto(`${baseUrl}`);
-//     await page.waitForSelector("canvas.particles-canvas-home")
-//     const html = await page.$eval('canvas.particles-canvas-home', e => { 
-//       style = e.currentStyle || window.getComputedStyle(e, false)
-//       return style.backgroundImage.slice(4, -1).replace(/"/g, "")
-//     });
-//       expect(html).toBe(`${baseUrl}static/media/home_background.8393fbf0.jpg`);
-//   }, 16000);
-
-//   test('main h1 loads correctly', async () => {
-//     let page = await browser.newPage();
-//     await page.goto(`${baseUrl}`);
-//     await page.waitForSelector(".main-text-container")
-//     const html = await page.$eval('.main-text-container h1', e => e.innerHTML);
-//     expect(html).toBe('Jay Lim');
-//   }, 16000);
-
-//   // make sure resume url is working
-//   test('Footer Resume URL works correctly', async () => {
-//     let page = await browser.newPage();
-//     await page.goto(`${baseUrl}`);
-//     await page.waitForSelector(".section-footer .link-resume")
-//     const resumeURL = await page.$eval(".section-footer .link-resume", e => e.href)
-//     expect(resumeURL).toBe(`${baseUrl}Jay_Lim_Resume.pdf`)
-//   }, 16000);
-
-//   test('Footer Github URL works correctly', async () => {
-//     let page = await browser.newPage();
-//     await page.goto(`${baseUrl}`);
-//     await page.waitForSelector(".section-footer .link-github")
-//     const resumeURL = await page.$eval(".section-footer .link-github", e => e.href)
-//     expect(resumeURL).toBe(`https://www.github.com/jlxc106`)
-//   }, 16000);
-
-//   test('Header LinkedIn URL works correctly', async () => {
-//     let page = await browser.newPage();
-//     await page.goto(`${baseUrl}`);
-//     await page.waitForSelector(".main-text-container .link-linkedin")
-//     const resumeURL = await page.$eval(".main-text-container .link-linkedin", e => e.href)
-//     expect(resumeURL).toBe(`https://www.linkedin.com/in/jayl1m/`)
-//   }, 16000);
-
-
-//   test('Header Github URL works correctly', async () => {
-//     let page = await browser.newPage();
-//     await page.goto(`${baseUrl}`);
-//     await page.waitForSelector(".main-text-container .link-github")
-//     const resumeURL = await page.$eval(".main-text-container .link-github", e => e.href)
-//     expect(resumeURL).toBe(`https://www.github.com/jlxc106`)
-//   }, 16000);
-// });
+  expect(container.textContent.includes("Jay Lim")).toBe(true)
+});
