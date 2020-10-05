74b0ebca73eb3664d1a2ba753ecf442a9fbbda98
Jay Lim <jaylim106@gmail.com>
2020-10-05 13:35:58 -0700

    update sh script

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,12 @@ eval "$(ssh-agent -s)"
 chmod 600 ./deploy_key
 echo -e "Host $SERVER_IP_ADDRESS\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 ssh-add ./deploy_key
-ssh -i ./deploy_key root@159.203.99.76 pwd
-echo "deploy.sh running..."
-cd /var/www/react-jayclim
-git pull origin master
-npm run build
-service apache2 restart
+ssh -i ./deploy_key -t -t root@159.203.99.76 << EOF
+    pwd
+    echo "deploy.sh running..."
+    cd /var/www/react-jayclim
+    pwd
+    git pull origin master
+    npm run build
+    service apache2 restart
+EOF
