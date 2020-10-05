2efb9d03d9c6f7826620ef1ca800b27fa9f12315
Jay Lim <jaylim106@gmail.com>
2020-10-05 12:53:56 -0700

    testing deploy script

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import Home from "./Home";
 import "./App.css";
 
-function App(props) { 
+function App(props) {
   return (
     <div className="App">
       <Router>
         <Switch>
-            <Route path="/" component={Home} />
-        </Switch>
-      </Router>
+          <Route path="/" component={Home} />{" "}
+        </Switch>{" "}
+      </Router>{" "}
     </div>
   );
 }
