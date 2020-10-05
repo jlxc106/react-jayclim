f9348fa64881b91c09927c3c5d7d81a1fc873981
Jay Lim <jaylim106@gmail.com>
2020-10-05 13:28:28 -0700

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
+openssl aes-256-cbc -K $encrypted_dfdcfd5172af_key -iv $encrypted_dfdcfd5172af_iv -in deploy_key.enc -out ./deploy_key -d
+eval "$(ssh-agent -s)"
+chmod 600 ./deploy_key
+echo -e "Host $SERVER_IP_ADDRESS\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+ssh-add ./deploy_key
 ssh -i ./deploy_key root@159.203.99.76 pwd
 echo "deploy.sh running..."
 cd /var/www/react-jayclim
